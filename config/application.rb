11768e10950bb36903828be501a001746ef6506c
Chase Clettenberg <clettenberg@gmail.com>
2020-03-25 19:46:22 -0500

    Add restaurant table

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,6 @@ module CovidCarryOut
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.active_record.schema_format = :sql
   end
 end
---
+++ db/migrate/20200326004321_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :website
+      t.string :hours
+
+      t.timestamps
+    end
+  end
+end
