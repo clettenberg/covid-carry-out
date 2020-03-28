222a0a7ce26410c918789d8c4fa43d5654515709
Chase Clettenberg <clettenberg@gmail.com>
2020-03-28 16:34:07 -0500

    Move data fetch into App component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,16 +1,51 @@
+import { fetch } from 'whatwg-fetch'
 import React from 'react'
 import Restaurants from './Restaurants'
 import { Navbar } from 'react-bootstrap'
 
-const App = () => (
-  <>
-    <Navbar bg='light'>
-      <Navbar.Brand href='#home'>Covid Carry Out - STL</Navbar.Brand>
-    </Navbar>
-    <div style={{ marginTop: '20px' }} className='container'>
-      <Restaurants />
-    </div>
-  </>
-)
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      restaurants: [],
+      err: []
+    }
+  }
+
+  fetchRestaurants () {
+    return fetch('/api/restaurants')
+      .then(results => results.json())
+      .then(results => {
+        return results.errors
+          ? Promise.reject(Error.new('Error fetching restaurants'))
+          : Promise.resolve(results)
+      })
+  }
+
+  componentDidMount () {
+    this.fetchRestaurants()
+      .then(restaurants => {
+        this.setState({ restaurants })
+      })
+  }
+
+  render () {
+    const {
+      restaurants
+    } = this.state
+
+    return (
+      <>
+        <Navbar bg='light'>
+          <Navbar.Brand href='#home'>Covid Carry Out - STL</Navbar.Brand>
+        </Navbar>
+        <div style={{ marginTop: '20px' }} className='container'>
+          <Restaurants restaurants={restaurants} />
+        </div>
+      </>
+    )
+  }
+}
 
 export default App
